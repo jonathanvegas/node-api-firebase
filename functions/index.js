34988e6a023109f401f84121edab3d561713e627
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-28 16:28:46 -0400

    added delete feature

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,10 @@ app.get('/another-one', (req,res) => {
   res.send('This one works, too!');
 });
 
+app.delete('/delete-one', (req, res) => {
+  Array.splice(3,5)
+})
+
 exports.api = functions.https.onRequest(app);
 
 exports.helloThere = functions.https.onRequest((req,res) => {
