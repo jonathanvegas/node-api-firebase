f3dae519120fc7f4cad4814c32f903820c6189e0
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-28 15:48:44 -0400

    push to firebase cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,21 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+//setup routes...
+app.get('/test', (req,res) => {
+  res.send('Wow, it actually worked!');
+});
+
+app.get('/another-one', (req,res) => {
+  res.send('This one works, too!');
+});
+
+
+exports.api = functions.https.onRequest(app);
 
 exports.helloThere = functions.https.onRequest((req,res) => {
   res.send('Hello there Firebase Cloud user!');
